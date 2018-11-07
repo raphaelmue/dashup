1e766af0fb0cf02c38405feca052135a256128b8
Raphael Müßeler <raphael.muesseler@sap.com>
2018-11-07 16:04:41 +0100

    architecture: implemented hello world with jsp files

--- de.dashup.application/src/main/java/de/dashup/application/DashupApplication.java
+++ de.dashup.application/src/main/java/de/dashup/application/DashupApplication.java
@@ -2,9 +2,17 @@ package de.dashup.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DashupApplication {
+public class DashupApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DashupApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DashupApplication.class, args);
     }
--- de.dashup.application/src/main/java/de/dashup/application/DashupController.java
+++ de.dashup.application/src/main/java/de/dashup/application/DashupController.java
@@ -1,15 +1,17 @@
 package de.dashup.application;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DashupController {
 
-    @RequestMapping("/dashup")
-    public ModelAndView main() {
-        System.out.println("dashup controller.");
-        return new ModelAndView("index");
+    @RequestMapping("/hello")
+    public String main(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        System.out.println("dashup controller");
+        model.addAttribute("name", name);
+        return "index";
     }
 }
