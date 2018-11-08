7163208abe3596f849b9eb9047d5ab3a87052891
Joshua Schulz <joshua.schulz@sap.com>
2018-11-08 14:15:56 +0100

    cucumber added dummy implementations of tests for layout features and corrected senario to senario outline

---
+++ de.dashup.application/src/test/java/de.dashup.test/steps/ChangePanelStructStepdefs.java
@@ -0,0 +1,158 @@
+package de.dashup.test.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ChangePanelStructStepdefs {
+    @When("^The user clicks on the rearrangement button$")
+    public void the_user_clicks_on_the_rearrangement_button() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Dashup's general panel structure is now editable as change structure mode is activated$")
+    public void dashup_s_general_panel_structure_is_now_editable_as_change_structure_mode_is_activated() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^Change structure mode is activated$")
+    public void change_structure_mode_is_activated() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks on add section button$")
+    public void user_clicks_on_add_section_button() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^A pop-up will show up, asking the user for input$")
+    public void a_pop_up_will_show_up_asking_the_user_for_input() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User enters \"([^\"]*)\"$")
+    public void user_enters(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks on submit button$")
+    public void user_clicks_on_submit_button() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^A new empty section will be rendered, being titled with \"([^\"]*)\"$")
+    public void a_new_empty_section_will_be_rendered_being_titled_with(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks on the name of a section$")
+    public void user_clicks_on_the_name_of_a_section() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^section will be renamed to \"([^\"]*)\"$")
+    public void section_will_be_renamed_to(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User clicks on the remove icon of a section$")
+    public void user_clicks_on_the_remove_icon_of_a_section() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Section will be removed$")
+    public void section_will_be_removed() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^All panels will be moved to the section above$")
+    public void all_panels_will_be_moved_to_the_section_above() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User drags a panel to another section$")
+    public void user_drags_a_panel_to_another_section() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^All the other panel will reorder and the dragged panel is in the other section$")
+    public void all_the_other_panel_will_reorder_and_the_dragged_panel_is_in_the_other_section() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User drags a section to another position$")
+    public void user_drags_a_section_to_another_position() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Section will be reordered to the specified position with all its containing panels$")
+    public void section_will_be_reordered_to_the_specified_position_with_all_its_containing_panels() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User right clicks on a panel$")
+    public void user_right_clicks_on_a_panel() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Size menu will pop up containing the three options small, medium and large$")
+    public void size_menu_will_pop_up_containing_the_three_options_small_medium_and_large() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User selects menu entry \"([^\"]*)\"$")
+    public void user_selects_menu_entry(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^panel will be resized to \"([^\"]*)\"$")
+    public void panel_will_be_resized_to(String arg1) throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User has made changes$")
+    public void user_has_made_changes() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^All changes will come into effect$")
+    public void all_changes_will_come_into_effect() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^User has made a change$")
+    public void user_has_made_a_change() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^The latest change will be undone$")
+    public void the_latest_change_will_be_undone() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
