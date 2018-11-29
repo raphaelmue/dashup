cd194da404e1da579d418c46ba96a2e30ac5b5fe
Raphael Müßeler <raphael@muesseler.de>
2018-11-29 20:11:48 +0100

    panels: removed warnings in DashupBuilder and fixed floating of panels

--- de.dashup.model/src/main/java/de/dashup/model/builder/DashupBuilder.java
+++ de.dashup.model/src/main/java/de/dashup/model/builder/DashupBuilder.java
@@ -6,17 +6,14 @@ import de.dashup.shared.User;
 
 public class DashupBuilder {
 
-    private final PanelLoader panelLoader = PanelLoader.getInstance();
-
-
     public static String buildUsersPanels(User user) {
         StringBuilder content = new StringBuilder();
         for (Section s : user.getSections()) {
-            content.append("<div> <h3>" + s.getName() + "</h3> <hr/> <div>");
+            content.append("<div> <h3>").append(s.getName()).append("</h3> <hr/> <div>");
             for (Panel p : s.getPanels()) {
                 content.append(p.getHtmlContent());
             }
-            content.append("</div> <div class=\"clear-float\"></div> </div>");
+            content.append("<div class=\"clear-float\"></div></div></div>");
         }
         return content.toString();
     }
