5d661b636bea8a2af4d00038ac984edddeb60156
svenleonhard <40168082+svenleonhard@users.noreply.github.com>
2018-12-02 02:09:10 +0100

    drag and drop for generically generated panels

--- de.dashup.model/src/main/java/de/dashup/model/builder/DashupBuilder.java
+++ de.dashup.model/src/main/java/de/dashup/model/builder/DashupBuilder.java
@@ -20,14 +20,33 @@ public class DashupBuilder {
 
     public static String buildUsersPanelsLayoutMode(User user) {
         StringBuilder content = new StringBuilder();
-        //content.append("<div id=\"drag_container_section\">");
+        content.append("<div id=\"drag_container_section\">");
         for (Section s : user.getSections()) {
-
+            //content.append("<div class=\"dashup-section\" id=\"s").append(s.getId()).append("\">");
             content.append("<div class=\"dashup_section\" id=\"s").append(s.getId()).append("\">");
-            content.append("<p>test</p>");
-            content.append("</div");
+            content.append("<div><span class=\"handle\">#</span><span><input type=\"text\" name=\"txt\" value=\"");
+            content.append(s.getName());
+            content.append("\" onchange=\"myFunction(this.value)\"><button type=\"button\"  onclick=\"onDelete('s");
+            content.append(s.getId());
+            content.append("')\" class=\"btn btn-circle btn-lg\"><i class=\"glyphicon glyphicon-remove\"></i></button></span></div>");
+            content.append("<hr>");
+            content.append("<div class=\"drag_container\" id=\"drag_container");
+            content.append(s.getId()).append("\">");
+
+            for (Panel p : s.getPanels()) {
+
+                content.append("<div id=\"");
+                content.append(p.getId());
+                content.append("\" class=\"dashup_panel\">").append(p.getName());
+                //content.append(p.getHtmlContent());
+                content.append("</div>");
+            }
+
+            content.append("</div>");
+
+            content.append("</div>");
         }
-        //content.append("</div>");
+        content.append("</div>");
         System.out.println(content.toString());
         return content.toString();
 
