555196f4de4b392ed950eab2234ee67771ee779f
Joshua Schulz <joshua.schulz@sap.com>
2019-03-19 08:12:33 +0100

    unitTEsting DASHUP-86 removed surefire plugin to test if tests are running

--- de.dashup.model/src/test/java/de/dashup/test/DatabaseUnitTest.java
+++ de.dashup.model/src/test/java/de/dashup/test/DatabaseUnitTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
 import java.sql.SQLException;
 import java.util.HashMap;
