35e9fcc4e8a8c4ba1ddb54586a6ceba217f4e296
Raphael Müßeler <raphael.muesseler@sap.com>
2019-03-20 09:34:10 +0100

    issue #12 (DASHUP-89): removed unused controllers and views and created marketplace view

--- de.dashup.application/src/main/java/de/dashup/application/controllers/MarketplaceController.java
+++ de.dashup.application/src/main/java/de/dashup/application/controllers/MarketplaceController.java
@@ -1,17 +1,22 @@
 package de.dashup.application.controllers;
 
+import de.dashup.application.controllers.util.ControllerHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/marketplace")
 public class MarketplaceController {
 
     @RequestMapping("/")
-    public String main(Model model, HttpServletRequest request) {
-        return "marketplace";
+    public String marketplace(@CookieValue(name = "token", required = false) String token, Model model, HttpServletRequest request) throws SQLException {
+        return ControllerHelper.defaultMapping(token, request, model, "marketplace", user -> {
+            // tbd
+        });
     }
 }
