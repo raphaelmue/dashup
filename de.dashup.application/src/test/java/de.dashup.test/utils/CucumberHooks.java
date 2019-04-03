44270ba63fb949fd576107e47f960fc3c9d9fa6d
Joshua Schulz <joshua.schulz@sap.com>
2019-04-03 16:29:11 +0200

    issue62 needed to move driver setup to use it in stepdefinitions to create new window

--- de.dashup.application/src/test/java/de.dashup.test/utils/CucumberHooks.java
+++ de.dashup.application/src/test/java/de.dashup.test/utils/CucumberHooks.java
@@ -7,11 +7,6 @@ import de.dashup.test.steps.GeneralStepdefs;
 import de.dashup.util.string.Hash;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,7 +16,6 @@ import java.util.Map;
 import java.util.Properties;
 
 public class CucumberHooks {
-    private WebDriver driver;
 
     @Before(order = 1)
     public void doDatabaseSetup() throws SQLException {
@@ -67,19 +61,20 @@ public class CucumberHooks {
         Properties p = new Properties();
         p.load(is);
         String name = p.getProperty("test.browser");
+        WebDriver driver;
         if (name != null) {
             switch (name) {
                 case "chrome":
-                    driver = this.setUpChromeDriver();
+                    driver = DriverUtil.setUpChromeDriver();
                     break;
                 case "firefox":
-                    driver = this.setUpFirefoxDriver();
+                    driver = DriverUtil.setUpFirefoxDriver();
                     break;
                 default:
                     throw new IllegalArgumentException("The argument " + name + " was read from properties, but is not expected!");
             }
         } else {
-            driver = this.setUpChromeDriver();
+            driver = DriverUtil.setUpChromeDriver();
         }
         GeneralStepdefs.setDriver(driver);
     }
@@ -92,35 +87,5 @@ public class CucumberHooks {
     @After(order = 2)
     public void clearDatabase() throws SQLException{
         GeneralStepdefs.getDatabase().clearDatabase();
-    }
-
-    private WebDriver setUpChromeDriver() {
-        final DesiredCapabilities desiredChromeCapabilities = DesiredCapabilities.chrome();
-        final ChromeOptions chromeOptions = new ChromeOptions();
-
-        if (DriverUtil.getPathToChromeBinary() != null) {
-            chromeOptions.setBinary(DriverUtil.getPathToChromeBinary());
-        }
-
-        desiredChromeCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-        System.setProperty("webdriver.chrome.driver", DriverUtil.getDriverPath("chrome"));
-        WebDriver returningDriver = new ChromeDriver(desiredChromeCapabilities);
-        returningDriver.manage().window().maximize();
-        return returningDriver;
-    }
-
-    private WebDriver setUpFirefoxDriver() {
-        final DesiredCapabilities desiredFirefoxCapabilities = DesiredCapabilities.firefox();
-        final FirefoxOptions firefoxOptions = new FirefoxOptions();
-
-        if (DriverUtil.getPathToChromeBinary() != null) {
-            firefoxOptions.setBinary(DriverUtil.getPathToFirefoxBinary());
-        }
-
-        desiredFirefoxCapabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, firefoxOptions);
-        System.setProperty("webdriver.gecko.driver", DriverUtil.getDriverPath("firefox"));
-        WebDriver returningDriver = new FirefoxDriver(desiredFirefoxCapabilities);
-        returningDriver.manage().window().maximize();
-        return returningDriver;
     }
 }
--- de.dashup.application/src/test/java/de.dashup.test/utils/DriverUtil.java
+++ de.dashup.application/src/test/java/de.dashup.test/utils/DriverUtil.java
@@ -1,6 +1,13 @@
 package de.dashup.test.utils;
 
-public class DriverUtil {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+ class DriverUtil {
 
     private static final String CHROME_DRIVER_WINDOWS = "./src/test/resources/de/dashup/test/chromedriver.exe";
     private static final String CHROME_DRIVER_LINUX = "/usr/bin/chromedriver";
@@ -10,7 +17,7 @@ public class DriverUtil {
     private static final String GECKO_DRIVER_LINUX = "/usr/bin/geckodriver";
     private static final String PATH_TO_FIREFOX_BINARY = "/usr/bin/firefox";
 
-    public static String getDriverPath(String profile) {
+    private static String getDriverPath(String profile) {
         if (System.getProperty("os.name").toLowerCase().contains("windows")) {
             if (profile.equals("chrome")) {
                 return CHROME_DRIVER_WINDOWS;
@@ -29,7 +36,7 @@ public class DriverUtil {
         return "";
     }
 
-    public static String getPathToChromeBinary() {
+     private static String getPathToChromeBinary() {
         if (System.getProperty("os.name").toLowerCase().contains("linux")) {
             return PATH_TO_CHROME_BINARY;
         } else {
@@ -37,11 +44,41 @@ public class DriverUtil {
         }
     }
 
-    public static String getPathToFirefoxBinary() {
+    private  static String getPathToFirefoxBinary() {
         if (System.getProperty("os.name").toLowerCase().contains("linux")) {
             return PATH_TO_FIREFOX_BINARY;
         } else {
             return null;
         }
+    }
+
+    static WebDriver setUpChromeDriver() {
+        final DesiredCapabilities desiredChromeCapabilities = DesiredCapabilities.chrome();
+        final ChromeOptions chromeOptions = new ChromeOptions();
+
+        if (DriverUtil.getPathToChromeBinary() != null) {
+            chromeOptions.setBinary(DriverUtil.getPathToChromeBinary());
+        }
+
+        desiredChromeCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        System.setProperty("webdriver.chrome.driver", DriverUtil.getDriverPath("chrome"));
+        WebDriver returningDriver = new ChromeDriver(desiredChromeCapabilities);
+        returningDriver.manage().window().maximize();
+        return returningDriver;
+    }
+
+    static WebDriver setUpFirefoxDriver() {
+        final DesiredCapabilities desiredFirefoxCapabilities = DesiredCapabilities.firefox();
+        final FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+        if (DriverUtil.getPathToChromeBinary() != null) {
+            firefoxOptions.setBinary(DriverUtil.getPathToFirefoxBinary());
+        }
+
+        desiredFirefoxCapabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, firefoxOptions);
+        System.setProperty("webdriver.gecko.driver", DriverUtil.getDriverPath("firefox"));
+        WebDriver returningDriver = new FirefoxDriver(desiredFirefoxCapabilities);
+        returningDriver.manage().window().maximize();
+        return returningDriver;
     }
 }
