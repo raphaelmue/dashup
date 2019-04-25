8272cd4880cb1784767d7c14c081d83b702c1e88
Felix Hausberger <felix.hausberger@sap.com>
2019-04-25 19:33:43 +0200

    issue85: created sample dashboard

--- de.dashup.application/src/main/webapp/views/includes/webComponents.jsp
+++ de.dashup.application/src/main/webapp/views/includes/webComponents.jsp
@@ -1,10 +1,2 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
-
-<%-- include web components --%>
-<script type="text/javascript" src="../../components/dashupComponent.js"></script>
-<script type="text/javascript" src="../../components/dashupLayout.js"></script>
-<script type="text/javascript" src="../../components/dashupInput.js"></script>
-<script type="text/javascript" src="../../components/dashupButton.js"></script>
-<script type="text/javascript" src="../../components/dashupGridLayout.js" ></script>
-<script type="text/javascript" src="../../components/dashupLabel.js" ></script>
-<script type="text/javascript" src="../../components/dashupTextField.js"></script>
+<script type="module" src="../../libraries/dashup-loader/dashup-loader.js"></script>
---
+++ de.dashup.application/src/main/webapp/widgets/dashup-todo.js
@@ -0,0 +1,40 @@
+import {LitElement, html} from "https://unpkg.com/lit-element@2.1.0/lit-element.js?module";
+export class DashupTodo extends LitElement{
+
+    render() {
+        return html`
+            <dashup-grid-layout>
+                <dashup-text-input name="todo"
+                                   placeholder='Enter a task here...' 
+                                   layout='{"row": 1, "offset": 0, "size": 8}'> 
+                </dashup-text-input>
+                <dashup-button text="Add"
+                               mode="add"
+                               consumers='{"list": "todo"}' 
+                               producers="todo"
+                               layout='{"row": 1, "offset": 1, "size": 3}'>
+                </dashup-button>
+                <dashup-list name="list"
+                             selectable="true"
+                             layout='{"row": 2, "offset": 0, "size": 8}'>
+                </dashup-list>
+                <dashup-grid-layout layout='{"row": 2, "offset": 0, "size": 4}'>
+                    <dashup-button text="Delete"
+                                   mode="delete"
+                                   consumers='{"list": "list"}' 
+                                   producers="list"
+                                   layout='{"row": 1, "offset": 0, "size": 12}'>
+                    </dashup-button>
+                    <dashup-button text="Save"
+                                   mode="save"
+                                   producers="list" 
+                                   layout='{"row": 2, "offset": 0, "size": 12}'
+                                   disabled>
+                    </dashup-button>
+                </dashup-grid-layout>
+            </dashup-grid-layout>
+        `;
+    }
+
+}
+customElements.define("dashup-todo",DashupTodo);
