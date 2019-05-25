52648c138a0f9b4d94ca0be9916f771283a4f718
Raphael Müßeler <raphael@muesseler.de>
2019-05-25 13:45:47 +0200

    issue #109 (DASHUP-150): improved publishing sonar file and added badge to README

--- service/publish-to-sonar.sh
+++ service/publish-to-sonar.sh
@@ -7,5 +7,12 @@ mkdir -p ./de.dashup.shared/src/test/
 BRANCH_NAME=$(git symbolic-ref --short -q HEAD)
 echo "Current branch: ${BRANCH_NAME}"
 
-# Analyzing project
-mvn sonar:sonar -P sonar -Dsonar.projectKey=dashup -Dsonar.branch.name=${BRANCH_NAME}
+# Checking whether current branch is a Pull Request
+if [[ ${BRANCH_NAME} == PR-* ]] ; then
+    PR_NUMBER="${BRANCH_NAME//PR-/}"
+    echo "Pull Request number: ${PR_NUMBER}"
+    mvn sonar:sonar -P sonar -Dsonar.projectKey=dashup -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=${PR_NUMBER}
+else
+    mvn sonar:sonar -P sonar -Dsonar.projectKey=dashup -Dsonar.branch.name=${BRANCH_NAME}
+fi
+
